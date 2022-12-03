b7d6757c9c2a1f90eef33ce20ab81995e55df958
dwifakhri <fakhri.chusaini01@gmail.com>
2022-12-03 18:32:36 +0700

    test

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,36 +1,41 @@
-import { data } from 'autoprefixer'
-import React from 'react'
-import { useState, useEffect } from 'react'
-
+import React from "react";
+import { useState, useEffect } from "react";
 
 function Card(props) {
+  const [pokemonIndex, setPokemonIndex] = useState("");
+  const [name, setName] = useState("");
 
-  const [pokemonIndex, setPokemonIndex] = useState("")
-  const [name, setName] = useState("")
-  
-  useEffect(()=> {
-    setPokemon()
-  },[])
+  useEffect(() => {
+    setPokemon();
+  }, []);
 
   function setPokemon() {
-    const {name,url} = props
-    const pokemonIndex = url.split('/')[url.split('/').length - 2]
+    const { name, url } = props;
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
 
-    setPokemonIndex(pokemonIndex)
-    setName(name)
-    
-  }    
+    setPokemonIndex(pokemonIndex);
+    setName(name);
+  }
 
   return (
     <div>
-        <div className='shadow-lg border rounded-lg'>
-            <div className='text-center flex flex-col justify-center h-full'>
-            <img onClick={props.onNavigate} className='w-full h-32' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonIndex}.svg`}/>
-            <div onClick={props.onNavigate} className='bg-gray-200 font-bold w-full'>{props.name}</div>
-            </div>
+      <div className="shadow-lg border rounded-lg">
+        <div className="text-center flex flex-col justify-center h-full">
+          <img
+            onClick={props.onNavigate}
+            className="w-full h-32"
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonIndex}.svg`}
+          />
+          <div
+            onClick={props.onNavigate}
+            className="bg-gray-200 font-bold w-full"
+          >
+            {props.name}
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/Pages/PokemonList.jsx
+++ src/Pages/PokemonList.jsx
@@ -1,64 +1,59 @@
-import React from 'react'
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import { WithRouter } from 'Utils/Navigation'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { WithRouter } from "Utils/Navigation";
 
-
-import Layout from 'Components/Layout'
-import Card from 'Components/Card'
-
+import Layout from "Components/Layout";
+import Card from "Components/Card";
 
 function PokemonList(props) {
-
-  const [datas, setDatas] = useState([])
-  const [page, setPage] = useState(20)
-  const [limit, setLimit] = useState(0)
-  const [loading, setLoading] = useState(true)
-
+  const [datas, setDatas] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  function fetchData(){
-    
-      axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${limit}&limit=${page}`)
-    .then((res)=> {
-      const {results} = res.data
-      const newPage = page + 20
-      const newLimit = page - 20
-      const temp = [...datas]
-      temp.push(...results)
-      setDatas(temp) 
-      setPage(newPage)
-      setLimit(newLimit)
-      
-    })
-    .catch((err)=> {
-      alert.apply(err.toString())
-    })
-    .finally(() => {
-      setLoading(false)
-    })
+  function fetchData() {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=${20}`)
+      .then((res) => {
+        const { results } = res.data;
+        const newPage = page + 20;
+        const temp = [...datas];
+        temp.push(...results);
+        setDatas(temp);
+        setPage(newPage);
+      })
+      .catch((err) => {
+        alert.apply(err.toString());
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }
-
 
   return (
     <Layout>
-        <div className='grid grid-cols-3 gap-3 px-3 py-2'>
-          {datas.map((data) => (
-            <Card
-            key={data.id}
+      <div className="grid grid-cols-3 gap-3 px-3 py-2">
+        {datas.map((data) => (
+          <Card
+            key={data.name}
             name={data.name}
             url={data.url}
-            onNavigate={()=>props.navigate(`/pokemon/${data.name}`)}
-            />
-
-          ))} 
-        </div>
-        <button onClick={()=>fetchData()} className='w-1/4 m-3 p-2 border-black border'>Load More Pokemon</button>
+            onNavigate={() => props.navigate(`/pokemon/${data.name}`)}
+          />
+        ))}
+      </div>
+      <button
+        onClick={() => fetchData()}
+        className="w-1/4 m-3 p-2 border-black border"
+      >
+        Load More Pokemon
+      </button>
     </Layout>
-  )
+  );
 }
 
-export default WithRouter(PokemonList)
+export default WithRouter(PokemonList);
